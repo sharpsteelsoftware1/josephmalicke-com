798d88daf712857319b15f6051158bfdfe19a256
Joseph Malicke <josephmalicke@centura.org>
2022-12-30 19:53:05 -0600

    NextJS 13, app directory, tailwind

---
+++ app/head.js
@@ -0,0 +1,8 @@
+export default function Head() {
+  return (
+    <>
+      <title>Joseph Malicke</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+    </>
+  );
+}
---
+++ app/page.js
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1 className="underline">Hello, Joseph Malicke!</h1>;
+}
