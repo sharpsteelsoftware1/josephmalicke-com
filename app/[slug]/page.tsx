7e0e283b5806a6d6f6e2817c69f838657e1d87da
Joseph Malicke <josephmalicke@centura.org>
2023-02-06 10:45:22 -0600

    Working slug

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -3,7 +3,8 @@ import { makeJsx } from "../../components/feed";
 
 export default async function BlogPage({ params }: { params: any }) {
   const rawItem = await sanityItem(params.slug);
-  const component = makeJsx(false)(rawItem);
+  const component = makeJsx(rawItem);
   // return <>{makeJsx(false)(rawItem)}</>;
-  return <div>{JSON.stringify(rawItem, null, 2)}</div>;
+  // return <div>{JSON.stringify(rawItem, null, 2)}</div>;
+  return component;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,6 @@ import { makeJsx } from "../components/feed";
 
 export default async function BlogPage() {
   const items = await sanityItems();
-  const components = R.map(makeJsx(true))(items);
+  const components = R.map(makeJsx)(items);
   return <div className="flex flex-col space-y-2">{components}</div>;
 }
