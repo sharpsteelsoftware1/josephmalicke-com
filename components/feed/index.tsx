f270576074442872fadba4ea0fefd91ccefc7f1d
Joseph Malicke <josephmalicke@centura.org>
2023-02-06 10:44:05 -0600

    Passing test

--- components/feed/index.tsx
+++ components/feed/index.tsx
@@ -1,12 +1,22 @@
+// import { blogFull, blogTeaser } from "./types/blog";
+// import { videoFull, videoTeaser } from "./types/video";
+
+// const makeJsx = (isTeaser = false) => (sanityItem: any) => {
+//   if (sanityItem._type === "blog")
+//     if (isTeaser) return blogTeaser(sanityItem);
+//     else return blogFull(sanityItem);
+//   else if (isTeaser) return videoTeaser(sanityItem);
+//   else return videoFull(sanityItem);
+// };
+
+// export { makeJsx };
+
 import { blogFull, blogTeaser } from "./types/blog";
 import { videoFull, videoTeaser } from "./types/video";
 
-const makeJsx = (isTeaser = false) => (sanityItem: any) => {
-  if (sanityItem._type === "blog")
-    if (isTeaser) return blogTeaser(sanityItem);
-    else return blogFull(sanityItem);
-  else if (isTeaser) return videoTeaser(sanityItem);
-  else return videoFull(sanityItem);
+const makeJsx = (sanityItem: any) => {
+  if (sanityItem._type === "blog") return blogTeaser(sanityItem);
+  else return videoTeaser(sanityItem);
 };
 
 export { makeJsx };
