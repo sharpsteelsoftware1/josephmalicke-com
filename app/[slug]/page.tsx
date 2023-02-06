b0aa96ab343f32db247719d225164a0ed55e01c0
Joseph Malicke <josephmalicke@centura.org>
2023-02-06 10:22:21 -0600

    Slug test 2

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -14,5 +14,7 @@ import { sanityItem } from "../../lib/sanity";
 import { makeJsx } from "../../components/feed";
 
 export default async function BlogPage({ params }: { params: any }) {
-  return <div>{JSON.stringify(params, null, 2)}</div>;
+  const rawItem = await sanityItem(params.slug);
+  // return <>{makeJsx(false)(rawItem)}</>;
+  return <div>{JSON.stringify(rawItem, null, 2)}</div>;
 }
