7ccc4645670a07b4ae4e0f67cdbb78ebab4cface
Joseph Malicke <josephmalicke@centura.org>
2023-02-06 10:24:15 -0600

    Slug test 3

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,20 +1,7 @@
-// import { sanityItem } from "../../lib/sanity";
-// import { makeJsx } from "../../components/feed";
-
-// export default async function BlogPage({
-//   params,
-// }: {
-//   params: { slug: string };
-// }) {
-//   const rawItem = await sanityItem(params.slug);
-//   return <>{makeJsx(false)(rawItem)}</>;
-// }
-
 import { sanityItem } from "../../lib/sanity";
 import { makeJsx } from "../../components/feed";
 
 export default async function BlogPage({ params }: { params: any }) {
   const rawItem = await sanityItem(params.slug);
-  // return <>{makeJsx(false)(rawItem)}</>;
-  return <div>{JSON.stringify(rawItem, null, 2)}</div>;
+  return <>{makeJsx(false)(rawItem)}</>;
 }
