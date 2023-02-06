8cf084185f0ecac6eb09812b1f2279d0624ddd9b
Joseph Malicke <josephmalicke@centura.org>
2023-02-06 10:50:12 -0600

    Slug test

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -3,8 +3,10 @@ import { makeJsx } from "../../components/feed";
 
 export default async function BlogPage({ params }: { params: any }) {
   const rawItem = await sanityItem(params.slug);
-  const component = makeJsx(rawItem);
+  console.log(JSON.stringify(rawItem, null, 2));
+
+  // const component = makeJsx(rawItem);
   // return <>{makeJsx(false)(rawItem)}</>;
-  // return <div>{JSON.stringify(rawItem, null, 2)}</div>;
-  return component;
+  return <div>{JSON.stringify(rawItem, null, 2)}</div>;
+  // return component;
 }
