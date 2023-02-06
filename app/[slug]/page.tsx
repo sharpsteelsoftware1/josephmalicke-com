c0bdcfcc62f323f50b25544b2ee00ffb8acabacf
Joseph Malicke <josephmalicke@centura.org>
2023-02-06 10:14:53 -0600

    Remove slug

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,11 +1,18 @@
+// import { sanityItem } from "../../lib/sanity";
+// import { makeJsx } from "../../components/feed";
+
+// export default async function BlogPage({
+//   params,
+// }: {
+//   params: { slug: string };
+// }) {
+//   const rawItem = await sanityItem(params.slug);
+//   return <>{makeJsx(false)(rawItem)}</>;
+// }
+
 import { sanityItem } from "../../lib/sanity";
 import { makeJsx } from "../../components/feed";
 
-export default async function BlogPage({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const rawItem = await sanityItem(params.slug);
-  return <>{makeJsx(false)(rawItem)}</>;
+export default async function BlogPage({}: {}) {
+  return <div>hello</div>;
 }
