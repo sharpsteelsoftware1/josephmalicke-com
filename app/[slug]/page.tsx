e4ecf208f3608b5ab8456e7c7efb7a1f39c7a88e
Joseph Malicke <josephmalicke@centura.org>
2023-02-06 18:27:50 -0600

    Tweak

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { sanityItem } from "../../lib/sanity";
 import { makeJsx } from "../../components/feed";
 
 export const dynamic = "force-dynamic";
+export const dynamicParams = true;
 
 export default async function BlogPage({
   params,
